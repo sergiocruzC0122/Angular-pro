444de612a63c18f950ddb6d9ec1848bfd9c2e1c4
Sergio Nicolas Cruz Cifuentes <sergio.cruz@onoff.com.co>
2025-12-22 16:59:49 -0500

    feat:You start thinking about components

--- 01-zoneless-calculator/src/app/features/calculator/pages/calculater-view/calculater-view.ts
+++ 01-zoneless-calculator/src/app/features/calculator/pages/calculater-view/calculater-view.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Calculator } from "../../components/calculator/calculator";
 
 @Component({
   selector: 'calculater-view',
-  imports: [],
+  imports: [Calculator],
   templateUrl: './calculater-view.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
