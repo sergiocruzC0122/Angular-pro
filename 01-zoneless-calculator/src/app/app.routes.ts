1d8dcf31985b44ef00c7aae51d08d59b81628e03
Sergio Nicolas Cruz Cifuentes <sergio.cruz@onoff.com.co>
2025-12-22 14:44:09 -0500

    feat:Create the calculator component and add the html code in view.

--- 01-zoneless-calculator/src/app/app.routes.ts
+++ 01-zoneless-calculator/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'calculator',
+        loadComponent: () => import('@/features/calculator/pages/calculater-view/calculater-view')
+    },
+    {
+        path: '**',
+        redirectTo: 'calculator',
+    },
+];
