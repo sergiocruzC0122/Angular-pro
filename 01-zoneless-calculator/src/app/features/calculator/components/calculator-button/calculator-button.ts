54e581c7da459dd0ab680779c9719872df8a9f38
Sergio Nicolas Cruz Cifuentes <sergio.cruz@onoff.com.co>
2026-01-05 15:25:00 -0500

    feat: Implement calculator component with button functionality and styles

---
+++ 01-zoneless-calculator/src/app/features/calculator/components/calculator-button/calculator-button.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  imports: [],
+  templateUrl: './calculator-button.html',
+  styleUrls: ['./calculator-button.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    'class': 'w-1/4 border-r border-b border-indigo-400'
+  }
+})
+export class CalculatorButton { 
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  public isDobleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDobleSize();
+  }
+}
---
+++ 01-zoneless-calculator/src/app/features/calculator/components/calculator/calculator.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButton } from "../calculator-button/calculator-button";
+
+@Component({
+  selector: 'calculator',
+  imports: [
+    CalculatorButton
+  ],
+  templateUrl: './calculator.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Calculator { }
