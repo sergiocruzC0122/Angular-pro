b7092da452d01d7a769f0ccb9007807d91b71a05
Sergio Nicolas Cruz Cifuentes <sergio.cruz@onoff.com.co>
2026-01-14 10:35:54 -0500

    feat: Created the components and the navigation bar.

---
+++ 02-pokemon-ssr/src/app/pages/pricing-page/pricing-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPage { }
